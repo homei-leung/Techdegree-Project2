de3681f1d0ebbd011997e38ec7f2f337ab2f93cf
homei-leung <49667943+homei-leung@users.noreply.github.com>
2019-05-26 19:44:23 -0400

    Revisions to matchInput function
    matchInput function outputs the correct resultsArray and appends the correct number of pages. Does not show the correct page or content.

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ indicate the desired number of students to display on each page.
 
 var fullList = document.getElementsByClassName('student-item cf');
 const displayItems = 10;
+var i;
 
 /***
 The showPage function calculates and shows the appropriate students for the
@@ -32,7 +33,6 @@ current page.
 function showPage(list, page){
   let startIndex = (page * displayItems) - displayItems;
   let endIndex = (page * displayItems);
-  var i;
   for (i = 0; i < list.length; i += 1) {
     if (i >= startIndex && i < endIndex){
     list[i].style.display = 'block'
@@ -58,7 +58,6 @@ function appendPageLinks(list){
   let ul = document.createElement('ul');
   pageLinks.appendChild(pagDiv);
   pagDiv.appendChild(ul);
-  var i;
 
   for (i = 1; i <= numPages; i += 1){
       let li = document.createElement('li');
@@ -70,7 +69,7 @@ function appendPageLinks(list){
 
       a.addEventListener("click", (event) => {
       let links = document.getElementsByTagName('a');
-      for(var i=0; i < links.length; i += 1){
+      for(i=0; i < links.length; i += 1){
         links[i].classList.remove("active");
       }
       event.target.className = "active";
@@ -83,32 +82,39 @@ function appendPageLinks(list){
 showPage(fullList, 1);
 appendPageLinks(fullList);
 
+//Make an array of all names in fullList.
+let studentNames = document.querySelectorAll('h3');
+let studentNamesText = [];
+for(i = 0; i < studentNames.length; i += 1){
+  studentNamesText.push(studentNames[i].textContent.toLowerCase());
+}
+
 /***
-Add functionality to search bar.
-Test input values to match list items. use tolowercase
-Store in a new array.
+Add functionality to search bar. Compare search query text with text in
+studentNamesText array. Hides index of array that do not include search
+query and will display "No results" if there are no matches.
 ***/
-function matchInput(searchInput, names){
-  for (i = 1; i < names.length; i += 1){
-    names[i];
-    if(searchInput.value.length !== 0 &&
-      names[i].textContent.toLowerCase().includes(searchInput.value.toLowerCase())){
-      names[i].classList.add('match');
+
+function matchInput() {
+  let searchQuery = input.value.toLowerCase();
+  let resultsArray = [];
+  for (i = 0; i < studentNamesText.length; i += 1){
+    if(studentNamesText[i].includes(searchQuery)){
+      resultsArray.push(fullList[i]);
     }
   }
-
-  button.addEventListener("click", (event) => {
-    let input = document.getElementById('input').textContent;
-    matchInput(input, fullList);
-    let searchResults = document.getElementsByClassName('match');
-    if(searchResults.length > 0){
-    appendPageLinks(searchResults);
-    }
-    if(searchResults.length === 0){
+console.log(resultsArray);
+    if(resultsArray.length == 0){
+      resultsArray[i].style.display = "none";
       let p = document.createElement('p');
       let page = document.querySelector('.page');
       page.appendChild(p);
       p.textContent = "No results."
     }
-  });
+    showPage(resultsArray, 1);
+    appendPageLinks(resultsArray);
 }
+
+button.addEventListener("click", (event) => {
+  matchInput();
+})
